1eb8b87797b82c0c6988e394d6f5c78f06aff6bb
Marti Raudsepp <marti@juffo.org>
2013-06-05 23:46:01 +0300

    Add tests from Unicode TR10, gives unexpected results
    Tests from Unicode Technical Report #10:
    http://www.unicode.org/reports/tr10/
    
    The results are inconsistent with orderings given in the spec, however.

--- test/sql/collate.sql
+++ test/sql/collate.sql
@@ -1,9 +1,38 @@
 CREATE EXTENSION musicbrainz_collate;
 
 -- Warning: Older ICU versions (before 4.8 or 4.6) may give different results
-SELECT * FROM (VALUES('AAA'), ('äää'), ('ÄÄÄ'), ('aaa')) AS x(txt)
-    ORDER BY musicbrainz_collate(txt);
+SELECT * FROM unnest('{AAA,äää,ÄÄÄ,aaa}'::text[]) AS txt ORDER BY musicbrainz_collate(txt);
 SELECT musicbrainz_collate('foobar');
 SELECT musicbrainz_collate('ľščťžýáí');
 SELECT musicbrainz_collate('');
 SELECT musicbrainz_collate(null);
+
+-- Additional tests from Unicode Technical Report #10
+-- http://www.unicode.org/reports/tr10/
+SELECT * FROM unnest(E'{
+    de luge,
+    de Luge,
+    de\u002dluge,
+    de\u002dLuge,
+    de\u2010luge,
+    de\u2010Luge,
+    death,
+    deluge,
+    deLuge,
+    demark,
+    ☠happy,
+    ☠sad,
+    ♡happy,
+    ♡sad
+}'::text[]) AS txt ORDER BY musicbrainz_collate(txt);
+
+SELECT * FROM unnest('{
+    role,
+    roles,
+    rule,
+    rôle,
+    Role,
+    rôle,
+    “role”,
+    ro□le
+}'::text[]) AS txt ORDER BY musicbrainz_collate(txt);
